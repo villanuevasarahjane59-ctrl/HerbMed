f919d8533ffc0f9089a5cb5d435dfffae9b8e344
villanuevasarahjane59-ctrl <villanuevasarahjane59@gmail.com>
2025-11-25 23:04:09 +0800

    Fix herb images to use hardcoded static paths - restart proof

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import CustomUser, Herb
+from django.http import HttpResponseRedirect
+from django.urls import path
+from django.shortcuts import render
+from .models import Herb, CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
@@ -8,12 +11,36 @@ class CustomUserAdmin(admin.ModelAdmin):
 
 @admin.register(Herb)
 class HerbAdmin(admin.ModelAdmin):
-    list_display = ['name', 'condition', 'scientific_name', 'created_at', 'has_image']
+    list_display = ['name', 'scientific_name', 'condition', 'created_at']
     list_filter = ['condition', 'created_at']
     search_fields = ['name', 'scientific_name']
     readonly_fields = ['created_at']
     
-    def has_image(self, obj):
-        return bool(obj.image or obj.image_url)
-    has_image.boolean = True
-    has_image.short_description = 'Has Image'
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('fix-images/', self.fix_images_view, name='fix_herb_images'),
+        ]
+        return custom_urls + urls
+    
+    def fix_images_view(self, request):
+        if request.method == 'POST':
+            herbs = Herb.objects.all()
+            updated_count = 0
+            
+            for herb in herbs:
+                if herb.image or herb.image_url:
+                    herb.image = None
+                    herb.image_url = ''
+                    herb.save()
+                    updated_count += 1
+            
+            self.message_user(request, f'Successfully updated {updated_count} herbs to use hardcoded static images!')
+            return HttpResponseRedirect('../')
+        
+        return render(request, 'admin/fix_images.html', {
+            'title': 'Fix Herb Images',
+            'herb_count': Herb.objects.count()
+        })
+
+    change_list_template = 'admin/herb_changelist.html'
